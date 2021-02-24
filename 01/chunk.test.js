b3e7ecb0c42ec695da6e071f058c2f5f6f0843ff
Arjan <arjanhoek5191@gmail.com>
2021-02-24 22:44:21 +0100

    changed folder structure

---
+++ 01/chunk.test.js
@@ -0,0 +1,12 @@
+// const chunkArray = require("./chunk");
+
+// test("chunkArray function exists", () => {
+//   expect(chunkArray).toBeDefined();
+// });
+
+// test('Cuts array with 11 in chunks of length 3', () => {
+//   const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//   const expectedOutput = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11]];
+//   const length = 3;
+//   expect(chunkArray(input, length)).toEqual(expectedOutput);
+// })
---
+++ 01/isAnagram.test.js
@@ -0,0 +1,18 @@
+// const isAnagram = require("./isAnagram");
+
+// test("isAnagram is a function type", () => {
+//   expect(typeof isAnagram).toEqual("function");
+// });
+
+// test('String is anagram 1', () => {
+//   const string1 = "Tablet";
+//   const string2 = "Battle"
+//   expect(isAnagram(string1, string2)).toBeTruthy()
+// });
+
+// test('String is anagram 2', () => {
+//   const string1 = "Hi there";
+//   const string2 = "Thy here"
+//   expect(isAnagram(string1, string2)).toBeFalsy()
+// });
+
